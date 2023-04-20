f184f1b4ec9f17d509c9aae9a525da54907d02df
unknown <jahidhasan815@gmail.com>
2023-04-20 15:10:16 -0400

    first commit

--- paperbox02/settings.py
+++ paperbox02/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#*32b8f-de0=z=i73@yb&nv4&+%!(f4q@sg&#m%pk0@lx*r18h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,8 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
    os.path.join (BASE_DIR,"static")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', "static")
